f4a65ee5a0d7d9e0bfaaf1bf4d1c59dd5f9a6ec0
NguyenDuy04 <duynguyenvan.1506@gmail.com>
2026-03-27 16:43:17 +0700

    Init Project

---
+++ components/navbar.tsx
@@ -0,0 +1,34 @@
+function Navbar() {
+    return (
+        <div className="bg-white w-25 h-25">
+            <div>
+                <div>
+
+                    Free Shipping for Orders Over $99
+                </div>
+                <div>
+
+                    <span>+1(888)Techubster</span>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <span>
+                        EN
+                    </span>
+                </div>
+                <div>
+                    <span>USD</span>
+                </div>
+                <div>
+                    <button>
+                        <span>Login</span>
+                    </button>
+                    <button>Register</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
